606413746b5afe54201d40795bc2ce5434ec8d43
Satya <satishdodake1993@gmail.com>
2019-10-16 13:14:28 +0530

    16-10-19 aternoon

---
+++ FactoryPattern.cs
@@ -0,0 +1,85 @@
+﻿
+
+namespace DesignPattern
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+   public abstract  class FactoryPattern
+    {
+       public abstract IComputer getUserDecidedConfiguration(string product);
+       
+    }
+
+    public class ConcreateFactory : FactoryPattern
+    {
+        public override IComputer getUserDecidedConfiguration(string newProduct)
+        {
+          switch (newProduct)
+            {
+                case "PC":
+                    return new PC();
+                    break;
+                case "Laptop":
+                    return new Laptop();
+                    break;
+                case "Server":
+                    return new Server();
+                    break;
+                default:
+                    throw new Exception("Your Product Not Made in this Factory");
+            }
+        }
+    }
+    public interface IComputer
+    {
+        abstract void getConfiguration(string ram,string harddisk,string processor);
+    }
+    public class PC : IComputer
+    {
+       public string ram;
+        public string hardDisk;
+        public string processor;
+
+        public void getConfiguration(string ram, string hardDisk, string processor)
+        {
+            this.ram = ram;
+            this.hardDisk = hardDisk;
+            this.processor = processor;
+            Console.WriteLine("PC Congiguration \nRam {0} ,HardDisk {1}, Processor {2}", ram, hardDisk, processor);     
+        }
+
+    }
+    public class Laptop : IComputer
+    {
+        public string ram;
+        public string hardDisk;
+        public string processor;
+
+        public void getConfiguration(string ram, string hardDisk, string processor)
+        {
+            this.ram = ram;
+            this.hardDisk = hardDisk;
+            this.processor = processor;
+            Console.WriteLine("Laptop Congiguration \nRam : {0} ,HardDisk : {1}, Processor : {2}", ram, hardDisk, processor);
+
+        }
+
+    }
+    public class Server : IComputer
+    {
+        public string processor;
+        public string memory;
+        public string nic;
+
+        public void getConfiguration(string processor, string memory, string nic)
+        {
+            this.processor = processor;
+            this.memory = memory;
+            this.nic = nic;
+            Console.WriteLine("Server Congiguration \nProcessor : {0} , Memory : {1}, Nic : {2}", processor, memory, nic);
+
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,38 +6,84 @@ namespace DesignPattern
     {
        public static void Main(string[] args)
         {
-          Parallel.Invoke( ()=> firstMessage(),
-              ()=>secondMessage()
-              );
+            FactoryPattern concreateFactory = new ConcreateFactory();
+            Console.WriteLine("Available Product : 1.PC    2.Laptop   3. Server\n");
+            Console.WriteLine("Enter Product Which You Want ");
+            string product = Console.ReadLine();
+            switch (product)
+            {
+                case "PC":
+                    IComputer pcProduct = concreateFactory.getUserDecidedConfiguration("PC");
+                    pcProduct.getConfiguration("8Gb", "2Tb", "I5");
+                    break;
+                case "Laptop":
+                    IComputer laptopProduct = concreateFactory.getUserDecidedConfiguration("Laptop");
+                    laptopProduct.getConfiguration("4Gb", "1Tb", "I3");
+                    break;
+                case "Server":
+                    IComputer serverProduct = concreateFactory.getUserDecidedConfiguration("Server");
+                    serverProduct.getConfiguration("Intel Xeon", "120Tb", "Dual");
+                    break;
+            }
+           
 
-            /*
-            ////Static class Example
-            ///
-            double radius = 10;
-            double length = 3;
-            double width = 6;
-            Console.WriteLine(StaticClass.AreaOfCircle(radius));
-            Console.WriteLine(StaticClass.AreaOfRectangle(length, width));
-
-
-      /*      SigletonLazyWithSealed.SingletonNested threadSafeFirst = new SigletonLazyWithSealed.SingletonNested();
-            threadSafeFirst.Message("Using Sealed First");       
-            SigletonLazyWithSealed.SingletonNested threadSafeSecond = new SigletonLazyWithSealed.SingletonNested();
-            threadSafeFirst.Message("Using Sealed Second");
+        
 
 
 
-            /*Sigleton Program
-             *Singleton singletoneFirstMsg= Singleton.GetSingleton;
-             Singleton singletoneSecondMsg = Singleton.GetSingleton;
-             singletoneFirstMsg.Message("This if First Message");
-             singletoneSecondMsg.Message("This if Second Message");
-             */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            /*  Parallel.Invoke( ()=> firstMessage(),
+                  ()=>secondMessage()
+                  );
+
+                /*
+                ////Static class Example
+                ///
+                double radius = 10;
+                double length = 3;
+                double width = 6;
+                Console.WriteLine(StaticClass.AreaOfCircle(radius));
+                Console.WriteLine(StaticClass.AreaOfRectangle(length, width));
+
+
+          /*      SigletonLazyWithSealed.SingletonNested threadSafeFirst = new SigletonLazyWithSealed.SingletonNested();
+                threadSafeFirst.Message("Using Sealed First");       
+                SigletonLazyWithSealed.SingletonNested threadSafeSecond = new SigletonLazyWithSealed.SingletonNested();
+                threadSafeFirst.Message("Using Sealed Second");
+
+
+
+                /*Sigleton Program
+                 *Singleton singletoneFirstMsg= Singleton.GetSingleton;
+                 Singleton singletoneSecondMsg = Singleton.GetSingleton;
+                 singletoneFirstMsg.Message("This if First Message");
+                 singletoneSecondMsg.Message("This if Second Message");
+                 */
 
 
             Console.ReadKey();
         }
-        private static void firstMessage()
+     /*   private static void firstMessage()
         {
             ////   SingletonLazy threadSafeFirst = SingletonLazy.GetSingleton;
          
@@ -48,6 +94,6 @@ namespace DesignPattern
            //// SingletonLazy threadSafeSecond = SingletonLazy.GetSingleton;
           ////  threadSafeSecond.Message("This is Second Message");         
         }
-     
+     */
     }
 }
