49c6bcb8f322f06f88cad90a2082ba09264c7960
Satya <satishdodake1993@gmail.com>
2019-10-16 15:46:34 +0530

    16-10-19 4pm

--- Program.cs
+++ Program.cs
@@ -6,28 +6,26 @@ namespace DesignPattern
     {
        public static void Main(string[] args)
         {
-            FactoryPattern concreateFactory = new ConcreateFactory();
-            Console.WriteLine("Available Product : 1.PC    2.Laptop   3. Server\n");
-            Console.WriteLine("Enter Product Which You Want ");
-            string product = Console.ReadLine();
-            switch (product)
-            {
-                case "PC":
-                    IComputer pcProduct = concreateFactory.getUserDecidedConfiguration("PC");
-                    pcProduct.getConfiguration("8Gb", "2Tb", "I5");
-                    break;
-                case "Laptop":
-                    IComputer laptopProduct = concreateFactory.getUserDecidedConfiguration("Laptop");
-                    laptopProduct.getConfiguration("4Gb", "1Tb", "I3");
-                    break;
-                case "Server":
-                    IComputer serverProduct = concreateFactory.getUserDecidedConfiguration("Server");
-                    serverProduct.getConfiguration("Intel Xeon", "120Tb", "Dual");
-                    break;
-            }
-           
+            Developer developer = new Developer();
+            developer.employeeId = 101;
+            developer.employeeName = "Kiran";
+            developer.employeeSalary = 25000;
 
-        
+            Developer developerCloneObject = (Developer)developer.Clone();
+            developerCloneObject.employeeName = "Satya";
+            developerCloneObject.getDetails();
+
+            ////tester details
+            ///
+        /*    Tester tester = new Tester();
+            tester.employeeId = 105;
+            tester.employeeName = "Ramesh";
+            tester.employeeSalary = 30000;
+            Tester testerCloneObject = (Tester)tester.Clone();
+            testerCloneObject.employeeSalary = 50000;
+            testerCloneObject.getDetails();
+
+            */
 
 
 
@@ -38,6 +36,27 @@ namespace DesignPattern
 
 
 
+            /*Factory Pattern
+             * 
+             *FactoryPattern concreateFactory = new ConcreateFactory();
+              Console.WriteLine("Available Product : 1.PC    2.Laptop   3. Server\n");
+              Console.WriteLine("Enter Product Which You Want ");
+              string product = Console.ReadLine();
+              switch (product)
+              {
+                  case "PC":
+                      IComputer pcProduct = concreateFactory.getUserDecidedConfiguration("PC");
+                      pcProduct.getConfiguration("8Gb", "2Tb", "I5");
+                      break;
+                  case "Laptop":
+                      IComputer laptopProduct = concreateFactory.getUserDecidedConfiguration("Laptop");
+                      laptopProduct.getConfiguration("4Gb", "1Tb", "I3");
+                      break;
+                  case "Server":
+                      IComputer serverProduct = concreateFactory.getUserDecidedConfiguration("Server");
+                      serverProduct.getConfiguration("Intel Xeon", "120Tb", "Dual");
+                      break;
+              }
 
 
 
@@ -52,33 +71,46 @@ namespace DesignPattern
 
 
 
-            /*  Parallel.Invoke( ()=> firstMessage(),
-                  ()=>secondMessage()
-                  );
-
-                /*
-                ////Static class Example
-                ///
-                double radius = 10;
-                double length = 3;
-                double width = 6;
-                Console.WriteLine(StaticClass.AreaOfCircle(radius));
-                Console.WriteLine(StaticClass.AreaOfRectangle(length, width));
-
-
-          /*      SigletonLazyWithSealed.SingletonNested threadSafeFirst = new SigletonLazyWithSealed.SingletonNested();
-                threadSafeFirst.Message("Using Sealed First");       
-                SigletonLazyWithSealed.SingletonNested threadSafeSecond = new SigletonLazyWithSealed.SingletonNested();
-                threadSafeFirst.Message("Using Sealed Second");
 
 
 
-                /*Sigleton Program
-                 *Singleton singletoneFirstMsg= Singleton.GetSingleton;
-                 Singleton singletoneSecondMsg = Singleton.GetSingleton;
-                 singletoneFirstMsg.Message("This if First Message");
-                 singletoneSecondMsg.Message("This if Second Message");
-                 */
+
+
+
+
+
+
+
+
+
+
+              /*  Parallel.Invoke( ()=> firstMessage(),
+                    ()=>secondMessage()
+                    );
+
+                  /*
+                  ////Static class Example
+                  ///
+                  double radius = 10;
+                  double length = 3;
+                  double width = 6;
+                  Console.WriteLine(StaticClass.AreaOfCircle(radius));
+                  Console.WriteLine(StaticClass.AreaOfRectangle(length, width));
+
+
+            /*      SigletonLazyWithSealed.SingletonNested threadSafeFirst = new SigletonLazyWithSealed.SingletonNested();
+                  threadSafeFirst.Message("Using Sealed First");       
+                  SigletonLazyWithSealed.SingletonNested threadSafeSecond = new SigletonLazyWithSealed.SingletonNested();
+                  threadSafeFirst.Message("Using Sealed Second");
+
+
+
+                  /*Sigleton Program
+                   *Singleton singletoneFirstMsg= Singleton.GetSingleton;
+                   Singleton singletoneSecondMsg = Singleton.GetSingleton;
+                   singletoneFirstMsg.Message("This if First Message");
+                   singletoneSecondMsg.Message("This if Second Message");
+                   */
 
 
             Console.ReadKey();
---
+++ Prototype.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPattern
+{
+   public class Prototype
+    {
+    }
+    public interface IEmployee
+    {
+        IEmployee Clone();
+        void getDetails();
+    }
+    public class Developer : IEmployee
+    {
+       public int employeeId { set; get; }
+        public string employeeName { set; get; }
+        public int employeeSalary { get; set; }
+        public  IEmployee Clone()
+        {
+            ////This Is Shallow Copy of Developer
+             return (IEmployee)MemberwiseClone();
+          ////  Developer developer = (Developer)this.MemberwiseClone();
+          ////  developer.employeeName = (IEmployee)this.employeeName.Clone();
+          ////  return developer;
+        }
+        public void getDetails()
+        {
+            Console.WriteLine("Developer :\nId : {0}, Name : {1}, Salary :{2}", employeeId, employeeName, employeeSalary);
+        }
+    }
+
+    public class Tester : IEmployee
+    {
+        public int employeeId { set; get; }
+        public string employeeName { set; get; }
+        public int employeeSalary { get; set; }
+        public IEmployee Clone()
+        {
+             return (IEmployee)MemberwiseClone();
+            ///
+
+          ////  return (IEmployee)this.Clone();
+        }
+        public void getDetails()
+        {
+            Console.WriteLine("Tester : \nId : {0} Name {1} :{1} Salary :{2}", employeeId, employeeName, employeeSalary);
+        }
+    }
+}
